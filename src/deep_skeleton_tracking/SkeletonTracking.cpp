7a20bd34e6694c6776b1d663f44082d11ac777ce
Baptiste Busch <baptiste.busch@gmail.com>
2017-06-02 14:36:57 +0200

    Add GFlags dependencies

--- src/deep_skeleton_tracking/SkeletonTracking.cpp
+++ src/deep_skeleton_tracking/SkeletonTracking.cpp
@@ -1,5 +1,23 @@
 #include "SkeletonTracking.hpp"
 
+// Gflags in the command line terminal. Check all the options by adding the flag `--help`, e.g. `openpose.bin --help`.
+// Note: This command will show you flags for several files. Check only the flags for the file you are checking. E.g. for `openpose.bin`, look for `Flags from examples/openpose/openpose.cpp:`.
+// Debugging
+DEFINE_int32(logging_level,             3,              "The logging level. Integer in the range [0, 255]. 0 will output any log() message, while 255 will not output any."
+                                                        " Current OpenPose library messages are in the range 0-4: 1 for low priority messages and 4 for important ones.");
+// OpenPose
+DEFINE_string(model_pose,               "COCO",         "Model to be used (e.g. COCO, MPI, MPI_4_layers).");
+DEFINE_string(model_folder,             "models/",      "Folder where the pose models (COCO and MPI) are located.");
+DEFINE_string(net_resolution,           "656x368",      "Multiples of 16.");
+DEFINE_string(resolution,               "1280x720",     "The image resolution (display). Use \"-1x-1\" to force the program to use the default images resolution.");
+DEFINE_int32(num_gpu_start,             0,              "GPU device start number.");
+DEFINE_double(scale_gap,                0.3,            "Scale gap between scales. No effect unless num_scales>1. Initial scale is always 1. If you want to change the initial scale, "
+                                                        "you actually want to multiply the `net_resolution` by your desired initial scale.");
+DEFINE_int32(num_scales,                1,              "Number of scales to average.");
+// OpenPose Rendering
+DEFINE_double(alpha_pose,               0.6,            "Blending factor (range 0-1) for the body part rendering. 1 will show it completely, 0 will hide it.");
+
+
 SkeletonTracking::SkeletonTracking(bool debug)
 {
 	this->debug = debug;
@@ -14,6 +32,48 @@ SkeletonTracking::~SkeletonTracking()
 	if (this->debug) {
 		cv::destroyWindow("view");
 	}
+}
+
+op::PoseModel SkeletonTracking::gflagToPoseModel(const std::string& poseModeString)
+{
+    op::log("", op::Priority::Low, __LINE__, __FUNCTION__, __FILE__);
+    if (poseModeString == "COCO")
+        return op::PoseModel::COCO_18;
+    else if (poseModeString == "MPI")
+        return op::PoseModel::MPI_15;
+    else if (poseModeString == "MPI_4_layers")
+        return op::PoseModel::MPI_15_4;
+    else
+    {
+        op::error("String does not correspond to any model (COCO, MPI, MPI_4_layers)", __LINE__, __FUNCTION__, __FILE__);
+        return op::PoseModel::COCO_18;
+    }
+}
+
+// Google flags into program variables
+std::tuple<cv::Size, cv::Size, cv::Size, op::PoseModel> SkeletonTracking::gflagsToOpParameters()
+{
+    op::log("", op::Priority::Low, __LINE__, __FUNCTION__, __FILE__);
+    // outputSize
+    cv::Size outputSize;
+    auto nRead = sscanf(FLAGS_resolution.c_str(), "%dx%d", &outputSize.width, &outputSize.height);
+    op::checkE(nRead, 2, "Error, resolution format (" +  FLAGS_resolution + ") invalid, should be e.g., 960x540 ", __LINE__, __FUNCTION__, __FILE__);
+    // netInputSize
+    cv::Size netInputSize;
+    nRead = sscanf(FLAGS_net_resolution.c_str(), "%dx%d", &netInputSize.width, &netInputSize.height);
+    op::checkE(nRead, 2, "Error, net resolution format (" +  FLAGS_net_resolution + ") invalid, should be e.g., 656x368 (multiples of 16)", __LINE__, __FUNCTION__, __FILE__);
+    // netOutputSize
+    const auto netOutputSize = netInputSize;
+    // poseModel
+    const auto poseModel = gflagToPoseModel(FLAGS_model_pose);
+    // Check no contradictory flags enabled
+    if (FLAGS_alpha_pose < 0. || FLAGS_alpha_pose > 1.)
+        op::error("Alpha value for blending must be in the range [0,1].", __LINE__, __FUNCTION__, __FILE__);
+    if (FLAGS_scale_gap <= 0. && FLAGS_num_scales > 1)
+        op::error("Uncompatible flag configuration: scale_gap must be greater than 0 or num_scales = 1.", __LINE__, __FUNCTION__, __FILE__);
+    // Logging and return result
+    op::log("", op::Priority::Low, __LINE__, __FUNCTION__, __FILE__);
+    return std::make_tuple(outputSize, netInputSize, netOutputSize, poseModel);
 }
 
 void SkeletonTracking::imageCallback(const sensor_msgs::ImageConstPtr& msg)
--- src/deep_skeleton_tracking/SkeletonTracking.hpp
+++ src/deep_skeleton_tracking/SkeletonTracking.hpp
@@ -1,3 +1,6 @@
+#ifndef SKELETON__TRACKING_HPP
+#define SKELETON__TRACKING_HPP
+
 // C++ std library dependencies
 #include <atomic> // std::atomic
 #include <chrono> // `std::chrono::` functions and classes, e.g. std::chrono::milliseconds
@@ -7,9 +10,6 @@
 #include <vector> // std::vector
 // OpenCV dependencies
 #include <opencv2/core/core.hpp> // cv::Mat & cv::Size
-// Other 3rdpary depencencies
-#include <gflags/gflags.h> // DEFINE_bool, DEFINE_int32, DEFINE_int64, DEFINE_uint64, DEFINE_double, DEFINE_string
-#include <glog/logging.h> // google::InitGoogleLogging, CHECK, CHECK_EQ, LOG, VLOG, ...
 
 // OpenPose dependencies
 // Option a) Importing all modules
@@ -21,12 +21,19 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <cv_bridge/cv_bridge.h>
 
+// Other 3rdpary depencencies
+#include <gflags/gflags.h> // DEFINE_bool, DEFINE_int32, DEFINE_int64, DEFINE_uint64, DEFINE_double, DEFINE_string
+#include <glog/logging.h> // google::InitGoogleLogging, CHECK, CHECK_EQ, LOG, VLOG, ...
 
 class SkeletonTracking {
 private:
 	bool debug;
+	op::PoseModel gflagToPoseModel(const std::string& poseModeString);
+	std::tuple<cv::Size, cv::Size, cv::Size, op::PoseModel> gflagsToOpParameters();
 public:
 	SkeletonTracking(bool debug);
 	~SkeletonTracking();
 	void imageCallback(const sensor_msgs::ImageConstPtr& msg);
 };
+
+#endif // SKELETON__TRACKING_HPP
